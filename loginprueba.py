74dad03e3c6536e15948a8d620880b576eaf62d4
guidomrauber <49770318+guidomrauber@users.noreply.github.com>
2022-10-30 15:42:51 -0300

    primer commit diseñando login

---
+++ loginprueba.py
@@ -0,0 +1,24 @@
+import tkinter 
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+pantalla = Tk()
+pantalla.geometry("800x600")
+pantalla.title("BIENVENIDOS")
+pantalla.iconbitmap("images.ico")
+
+image = PhotoImage(file = "images.gif")
+image = image.subsample(1,1)
+label=Label(image=image)
+label.pack()
+Label(text="ACCESO AL SISTEMA",  fg="navy",width="300",height="1", font=("calibri", 25)).pack()
+
+Label(text="DEL GYM",  fg="navy",width="300",height="1", font=("calibri", 25)).pack()
+Label(text="").pack()
+Button(text="INICIAR SESION", width="30",height="3").pack()
+Label(text="").pack()
+Button(text="REGISTRAR", width="30",height="3").pack()
+
+pantalla.mainloop()
